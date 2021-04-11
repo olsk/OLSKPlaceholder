0182434c64050258f3ba3da949103962caaf6781
Rosano Coutinho <pub@xeari.com>
2021-04-11 11:15:46 -0300

    Fix typo

--- rollup-start.js
+++ rollup-start.js
@@ -2,11 +2,7 @@ import RollupStart from './main.svelte';
 
 const OLSKDetailPlaceholder = new RollupStart({
 	target: document.body,
-	props: Object.assign({
-		OLSKDetailPlaceholderDispatchStorage () {
-			window.TestOLSKDetailPlaceholderDispatchStorage.innerHTML = parseInt(window.TestOLSKDetailPlaceholderDispatchStorage.innerHTML) + 1;
-		},
-	}, Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries()))),
+	props: Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries())),
 });
 
 export default OLSKDetailPlaceholder;
