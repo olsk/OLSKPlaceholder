fe1e7176c1e9a264951111df2b3b5e0197772e30
Rosano Coutinho <pub@xeari.com>
2022-01-18 12:01:12 -0500

    Lint

--- ui-test-_access.js
+++ ui-test-_access.js
@@ -3,7 +3,7 @@ const kDefaultRoute = require('./controller.js').OLSKControllerRoutes().shift();
 Object.entries({
 	OLSKPlaceholder: '.OLSKPlaceholder',
 }).map(function (e) {
-	return global[e.shift()]  = e.pop();
+	return global[e.shift()] = e.pop();
 });
 
 describe('OLSKPlaceholder_Access', function () {
